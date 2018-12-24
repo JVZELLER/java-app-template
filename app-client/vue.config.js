54cc1ffea46cfefa5207712457f563ea7073c981
José Victor Zeller <josevictorzg@gmail.com>
2018-12-24 19:07:58 -0200

    chore: add app-client folders and configuration files

---
+++ app-client/vue.config.js
@@ -0,0 +1,19 @@
+// vue.config.js
+module.exports = {
+  // proxy all webpack dev-server requests starting with "/api"
+  // to our Spring Boot backend (localhost:9999) using http-proxy-middleware
+  // see https://cli.vuejs.org/config/#devserver-proxy
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9999',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  },
+  // Change build paths to make them Maven compatible
+  // see https://cli.vuejs.org/config/
+  outputDir: 'target/dist',
+  assetsDir: 'static'
+}
