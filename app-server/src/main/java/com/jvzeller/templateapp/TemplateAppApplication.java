94a743c96befb4cab6549acd4f99dcf0e02e7174
José Victor Zeller <josevictorzg@gmail.com>
2018-12-24 19:17:43 -0200

    chore: rename packages

--- app-server/src/main/java/com/jvzeller/templateapp/TemplateAppApplication.java
+++ app-server/src/main/java/com/jvzeller/templateapp/TemplateAppApplication.java
@@ -1,4 +1,4 @@
-package com.javatemplateapp.templateapp;
+package com.jvzeller.templateapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
