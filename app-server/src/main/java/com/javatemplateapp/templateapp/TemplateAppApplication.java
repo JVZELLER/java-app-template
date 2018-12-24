1d73457319364ee644f884d012f6f0e1d6fd544d
José Victor Zeller <josevictorzg@gmail.com>
2018-12-24 19:08:30 -0200

    chore: add app-server folders and configuration files

---
+++ app-server/src/main/java/com/javatemplateapp/templateapp/TemplateAppApplication.java
@@ -0,0 +1,14 @@
+package com.javatemplateapp.templateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateAppApplication.class, args);
+	}
+
+}
+
---
+++ app-server/src/test/java/com/javatemplateapp/templateapp/TemplateAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.javatemplateapp.templateapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TemplateAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
