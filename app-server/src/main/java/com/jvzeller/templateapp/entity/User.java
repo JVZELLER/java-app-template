f5b5932603c44e26a028f37bb2d92c34a52f62ae
José Victor Zeller <josevictorzg@gmail.com>
2018-12-25 13:36:48 -0200

    test: add some data to 'User' entity

--- app-server/src/main/java/com/jvzeller/templateapp/entity/User.java
+++ app-server/src/main/java/com/jvzeller/templateapp/entity/User.java
@@ -1,8 +1,29 @@
 package com.jvzeller.templateapp.entity;
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+
+@Entity
 public class User extends DomainEntity {
 	
-	public User() {
+	private String name;
+	private LocalDate birthDate;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+	
+	public void setBirthDay(LocalDate birthDate) {
+		this.birthDate = birthDate;
 	}
 
 }
