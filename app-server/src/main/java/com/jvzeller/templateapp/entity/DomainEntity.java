6c9e88994ddba2cfee77b3e0292b98374fb96008
José Victor Zeller <josevictorzg@gmail.com>
2018-12-24 22:22:36 -0200

    chore: add 'DomainEntity'

---
+++ app-server/src/main/java/com/jvzeller/templateapp/entity/DomainEntity.java
@@ -0,0 +1,50 @@
+package com.jvzeller.templateapp.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@Entity
+@JsonInclude(Include.NON_NULL)
+public abstract class DomainEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private long id;
+	private Boolean active;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
+	private LocalDateTime creationDate;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public LocalDateTime getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(LocalDateTime creationDate) {
+		this.creationDate = creationDate;
+	}
+
+}
---
+++ app-server/src/main/java/com/jvzeller/templateapp/entity/User.java
@@ -0,0 +1,8 @@
+package com.jvzeller.templateapp.entity;
+
+public class User extends DomainEntity {
+	
+	public User() {
+	}
+
+}
