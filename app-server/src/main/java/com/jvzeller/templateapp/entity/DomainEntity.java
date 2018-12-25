b89dcecd5a3934db0b8a66eec717aecdd9d7f75a
José Victor Zeller <josevictorzg@gmail.com>
2018-12-25 19:47:47 -0200

    chore: resolve hibernate inheritance strategy

--- app-server/src/main/java/com/jvzeller/templateapp/entity/DomainEntity.java
+++ app-server/src/main/java/com/jvzeller/templateapp/entity/DomainEntity.java
@@ -6,12 +6,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @Entity
+@Inheritance( strategy=InheritanceType.TABLE_PER_CLASS )
 @JsonInclude(Include.NON_NULL)
 public abstract class DomainEntity {
 	
