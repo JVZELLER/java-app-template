60736b2a0ddcdc2cf31d49a6c8a56474be00db9f
José Victor Zeller <josevictorzg@gmail.com>
2019-01-02 22:16:15 -0200

    chore: add 'axios' dependency'

--- app-client/src/main.js
+++ app-client/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import './plugins/vuetify'
-import './plugins/axios'
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
