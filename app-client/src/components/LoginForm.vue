2bfab7fc3c8d79f74f0fcfa4fff791a8ea6f20e1
José Victor Zeller <josevictorzg@gmail.com>
2019-01-13 16:41:48 -0200

    refactor: apply changes to 'LoginForme.vue'

--- app-client/src/components/LoginForm.vue
+++ app-client/src/components/LoginForm.vue
@@ -1,15 +1,14 @@
 <template>
   <v-layout align-center justify-center>
     <v-flex xs12 sm8 md4>
-      <v-card class="elevation-5">
+      <v-card class="elevation-12">
         <v-toolbar dark color="primary">
-          <v-toolbar-title>Login</v-toolbar-title>
+          <v-toolbar-title>App Template</v-toolbar-title>
         </v-toolbar>
         <v-card-text>
           <v-form>
             <v-text-field prepend-icon="person" name="login" label="Login" type="text"></v-text-field>
-            <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password">
-            </v-text-field>
+            <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
           </v-form>
         </v-card-text>
         <v-card-actions>
@@ -25,9 +24,6 @@
   export default {
     data: () => ({
       drawer: null
-    }),
-    props: {
-      source: String
-    }
+    })
   }
 </script>
