c47ea1a3852d0ce7395e42342d4417dc4c7a2853
José Victor Zeller <josevictorzg@gmail.com>
2019-07-08 13:51:18 -0300

    chore: change default layout

--- app-client/src/core/components/layout/toolbar/index.vue
+++ app-client/src/core/components/layout/toolbar/index.vue
@@ -1,16 +1,42 @@
-<template>
-  <v-container fluid fill-height>
-    <navigation-drawer/>
-    <toolbar/>
-    <v-content>
-      <slot/>
-    </v-content>
-  </v-container>
+<template id="toolbar">
+  <v-toolbar
+    :clipped-left="$vuetify.breakpoint.lgAndUp"
+    color="blue darken-3"
+    dark
+    app
+    fixed
+  >
+    <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <span class="hidden-sm-and-down">App Template</span>
+    </v-toolbar-title>
+    <v-text-field
+      flat
+      solo-inverted
+      hide-details
+      prepend-inner-icon="search"
+      label="Search"
+      class="hidden-sm-and-down"
+    ></v-text-field>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>apps</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>notifications</v-icon>
+    </v-btn>
+    <v-btn icon large>
+      <v-avatar size="32px" tile>
+        <img
+          src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+          alt="Vuetify"
+        >
+      </v-avatar>
+    </v-btn>
+  </v-toolbar>
 </template>
 
 <script>
-  import NavigationDrawer from './navigation-drawer'
-  import toolbar from './toolbar'
   export default {
     data: () => ({
       items: [
