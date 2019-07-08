82942f96e72274cdbf51175e3079b9ffe135b8a4
José Victor Zeller <josevictorzg@gmail.com>
2019-07-08 13:04:15 -0300

    chore: add layout and form validation data rules to 'store.js'

--- app-client/src/store.js
+++ app-client/src/store.js
@@ -5,23 +5,33 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    defaultLayout: 'default-layout'
+    defaultLayout: 'default-layout',
+    formValidation: true
   },
   getters: {
     DEFAULT_LAYOUT: state => {
       return state.defaultLayout
+    },
+    FORM_VALIDATION: state => {
+      return state.formValidation
     }
 
   },
   mutations: {
     SET_DEFAULT_LAYOUT: (state, payload) => {
       state.defaultLayout = payload
+    },
+    SET_FORM_VALIDATION: (state, payload) => {
+      state.formValidation = payload
     }
 
   },
   actions: {
     CHANGE_LAYOUT: (context, payload) => {
       context.commit('SET_DEFAULT_LAYOUT', payload)
+    },
+    CHANGE_FORM_VALIDATION: (context, payload) => {
+      context.commit('SET_FORM_VALIDATION', payload)
     }
   }
 })
