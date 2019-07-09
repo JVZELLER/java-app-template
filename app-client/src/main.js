d9fa88cbf680fa21a522f0c801e774f72ead26fa
José Victor Zeller <josevictorzg@gmail.com>
2019-07-09 13:31:53 -0300

    chore: add 'I18N' to 'main.js' file

--- app-client/src/main.js
+++ app-client/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import './plugins/vuetify'
+import './plugins/vuetify'
 import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import CoreComponents from '@/core/components'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
@@ -21,5 +23,6 @@ Vue.mixin({
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
